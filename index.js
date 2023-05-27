ce9c1b205f9e07805c24e2926752792b277346cf
Abhinav Aggarwal <abhinavaaggarwal01@gmail.com>
2023-05-27 22:44:32 +0530

    adding files for chrome extension

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const inputBtn = document.getElementById("btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("list")
-let myLeads = ["Lead 1", "Lead 2"]
+let myLeads = []
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    renderLeads()
+    inputEl.value = ""
 })
 
-for(let i=0;i<myLeads.length;i++){
-    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-    
-    //----create element----
-    const li = document.createElement("li")
-    //----set text content----
-    li.textContent = myLeads[i]
-    //----append to ul----
-    ulEl.append(li)
+function renderLeads() {
+    let listItems = ""
+    for(let i=0;i<myLeads.length;i++){
+        // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+
+        /*----create element----
+        const li = document.createElement("li")
+        ----set text content----
+        li.textContent = myLeads[i]
+        ----append to ul----
+        ulEl.append(li)
+        */
+        // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
 }
+
